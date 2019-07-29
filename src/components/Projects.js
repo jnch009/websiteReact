cd8270515ad0f8bb0817e917d61d85b95994a005
jnch009 <jngcheng@sfu.ca>
2019-07-29 16:24:12 -0700

    Attempting to use routing, installed react-router-dom

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Card from "shards-react";
+import {Card, CardHeader} from "shards-react";
 
 function Projects(){
     return (
--- src/components/Website.js
+++ src/components/Website.js
@@ -4,18 +4,24 @@ import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router';
 
 function Website() {
   return (
-    <Breadcrumb>
+    <div>
+      <Breadcrumb>
       <BreadcrumbItem>Home</BreadcrumbItem>
       <BreadcrumbItem>About</BreadcrumbItem>
       <BreadcrumbItem>Projects</BreadcrumbItem>
       <BreadcrumbItem>Blog</BreadcrumbItem>
       <BreadcrumbItem>What's New</BreadcrumbItem>
-      <ButtonGroup className="btnGrp" size="small">
-        <Button>1</Button>
-        <Button>2</Button>
-        <Button>3</Button>
-      </ButtonGroup>
-    </Breadcrumb>
+        <ButtonGroup className="btnGrp" size="small">
+          <Button>1</Button>
+          <Button>2</Button>
+          <Button>3</Button>
+        </ButtonGroup>
+      </Breadcrumb>
+
+      <hr/>
+
+      <div>This is the home page</div>
+    </div>
   );
 }
 
