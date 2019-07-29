451f6f0cddf09398d2e32a872339d8f3d28e6395
jnch009 <jngcheng@sfu.ca>
2019-07-29 15:55:35 -0700

    Adding new component projects

---
+++ src/components/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from "shards-react";
+
+function Projects(){
+    return (
+        <Card>
+            <CardHeader>This is the Projects Page</CardHeader>
+        </Card>
+    )
+}
+
+export default Projects;
