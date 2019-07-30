8a8f1414aa6dac83dafd97a280f412c20574273a
jnch009 <jngcheng@sfu.ca>
2019-07-30 16:14:14 -0700

    Setting up MySQL connection

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,13 @@ import React from 'react';
 import {Card, CardHeader} from "shards-react";
 
 var mysql = require('mysql');
+var con = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_SCHEMA,
+    port: process.env.DB_PORT
+});
 
 function Projects(){
     return (
