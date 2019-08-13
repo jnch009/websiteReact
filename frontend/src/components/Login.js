3e47ecf4e53cf7fb5072af95c32cc66e21f650be
jnch009 <jngcheng@sfu.ca>
2019-08-12 17:56:05 -0700

    Adding functionality to press enter when password is in focus

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -14,6 +14,7 @@ class Login extends React.Component{
         this.onChangeUsername = this.onChangeUsername.bind(this);
         this.onChangePassword = this.onChangePassword.bind(this);
         this.handleLoginClick = this.handleLoginClick.bind(this);
+        this.enterClicked = this.enterClicked.bind(this);
     }
 
     onChangeUsername(e){
@@ -22,6 +23,12 @@ class Login extends React.Component{
 
     onChangePassword(e){
         this.setState({password: e.target.value});
+    }
+
+    enterClicked(e){
+        if (e.key === "Enter"){
+            this.handleLoginClick();
+        }
     }
 
     handleLoginClick(){
@@ -46,7 +53,7 @@ class Login extends React.Component{
             <div id="login">
                 <Link id="register" to='/register'>Create a new account</Link>
                 <div id="username">Username: <br/><input onChange={this.onChangeUsername} name="user" value={this.state.username}></input></div>
-                <div id="password">Password: <br/><input type="password" onChange={this.onChangePassword} name="pass" value={this.state.password}></input></div>
+                <div id="password">Password: <br/><input type="password" onKeyDown={this.enterClicked} onChange={this.onChangePassword} name="pass" value={this.state.password}></input></div>
                 <Button id="loginSubmit" onClick={this.handleLoginClick}>Submit</Button>
             </div>
         );
