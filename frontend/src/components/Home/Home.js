534c63ecb53cc6d32be3cf1fe0b06afc3bd96afb
jnch009 <jngcheng@sfu.ca>
2020-03-04 21:46:52 -0800

    shortid for key generation

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
+
 import "./Home.css";
 
 function Home() {
+  const sID = require("shortid");
   const gridLabels = ['About', 'Tekken 7', 'Blog'];
   const gridItems = [];
   for (let lbl of gridLabels) {
-    gridItems.push(<div className="homeGrid"><h1>{lbl}</h1></div>);
+    gridItems.push(<div className="homeGrid" key={sID.generate()}><h1>{lbl}</h1></div>);
   }
 
   return <div className="flexContainer">{gridItems}</div>;
