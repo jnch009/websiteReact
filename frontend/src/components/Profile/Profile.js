6b9cf49ab237d47a7813fdcde8468f2ef5b1740d
jnch009 <jngcheng@sfu.ca>
2020-03-05 21:46:03 -0800

    Updating loop logic

--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -14,24 +14,28 @@ class Profile extends React.Component {
         return results.json();
       })
       .then(data => {
-        const { id, displayName, name, emails, nickname, picture } = JSON.parse(
+        const { id, displayName, nickname, picture } = JSON.parse(
           data.userProfile
         );
         this.setState({
-          profileData: { id, displayName, name, emails, nickname, picture }
+          profileData: { id, displayName, nickname, picture }
         });
       });
   }
 
   render() {
-    const listID = this.state.profileData.id;
+    const profileArray = Object.keys(this.state.profileData).map((i, index) => {
+      if (i !== "id") {
+        if (i === "picture") {
+          return <img className="profilePic" src={this.state.profileData[i]}></img>;
+        }
+        return <div key={index}>{this.state.profileData[i]}</div>;
+      }
+    });
+
     return (
-      <div>
-        <ul>
-          <div key={listID}><h1>{this.state.profileData.displayName}</h1></div>
-          <div key={listID}><h5>{this.state.profileData.nickname}</h5></div>
-          <img src={this.state.profileData.picture}></img>
-        </ul>
+      <div className="profileStyling">
+        {profileArray}
       </div>
     );
   }
