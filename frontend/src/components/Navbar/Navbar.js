b4361b7b6a021436a3efb0b21172e9225166368e
jnch009 <jngcheng@sfu.ca>
2020-03-15 14:22:38 -0700

    Update styling of navbar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Breadcrumb, BreadcrumbItem } from "shards-react";
+import { Breadcrumb, BreadcrumbItem, Button } from "shards-react";
 
 import { useAuth0 } from "../../react-auth0-spa";
 
 import "./Navbar.css";
 
 // TODO: let's refactor this into a navbar component and then render it on each page
-
+const className = require("classnames");
+let buttonNav = className("buttonNav");
 function Navbar() {
   const { isAuthenticated } = useAuth0();
 
@@ -15,32 +16,34 @@ function Navbar() {
     <>
       <Breadcrumb>
         <div className="flex_1">
-          <BreadcrumbItem>
-            <Link to="/">Home</Link>
-          </BreadcrumbItem>
-          <BreadcrumbItem>
-            <Link to="/about">About</Link>
-          </BreadcrumbItem>
-          <BreadcrumbItem>
-            <Link to="/projects">Accomplishments</Link>
-          </BreadcrumbItem>
-          <BreadcrumbItem>
-            <Link to="/blog">Blog</Link>
-          </BreadcrumbItem>
+          <Link to="/">
+            <Button className={buttonNav}>Home</Button>
+          </Link>
+          <Link to="/about">
+            <Button className={buttonNav}>About</Button>
+          </Link>
+          <Link to="/projects">
+            <Button className={buttonNav}>Accomplishments</Button>
+          </Link>
+          <Link to="/blog">
+            <Button className={buttonNav}>Blog</Button>
+          </Link>
           {isAuthenticated ? (
-            <BreadcrumbItem>
-              <Link to="/profile">Profile</Link>
-            </BreadcrumbItem>
+            <Link to="/profile">
+              <Button className={buttonNav}>Profile</Button>
+            </Link>
           ) : null}
         </div>
         <div className="flex_2"></div>
-        <BreadcrumbItem>
-          {isAuthenticated ? (
-            <Link to="/logout">Logout</Link>
-          ) : (
-            <Link to="/login">Login</Link>
-          )}
-        </BreadcrumbItem>
+        {isAuthenticated ? (
+          <Link to="/logout">
+            <Button className={buttonNav}>Logout</Button>
+          </Link>
+        ) : (
+          <Link to="/login">
+            <Button className={buttonNav}>Login</Button>
+          </Link>
+        )}
         <hr />
       </Breadcrumb>
     </>
