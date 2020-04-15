210da38cc804fbe32857de3fb3db2a5bb0f3073f
jnch009 <jngcheng@sfu.ca>
2020-04-15 14:09:22 -0700

    Updating semantics of navbar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -14,36 +14,44 @@ function Navbar() {
   return (
     <>
       <Breadcrumb className="websiteBreadcrumb">
-        <div className="flex_1">
-          <Link to="/">
-            <Button className={buttonNav}>Home</Button>
-          </Link>
-          <Link to="/about">
-            <Button className={buttonNav}>About</Button>
-          </Link>
-          <Link to="/projects">
-            <Button className={buttonNav}>Accomplishments</Button>
-          </Link>
-          <Link to="/blog">
-            <Button className={buttonNav}>Blog</Button>
-          </Link>
-          {isAuthenticated ? (
-            <Link to="/profile">
-              <Button className={buttonNav}>Profile</Button>
+        <ul className="navbarContainer">
+          <li>
+            <Link to="/">
+              <Button className={buttonNav}>Home</Button>
             </Link>
-          ) : null}
-        </div>
-        <div className="flex_2"></div>
-        {isAuthenticated ? (
-          <Link to="/logout">
-            <Button className={buttonNav}>Logout</Button>
-          </Link>
-        ) : (
-          <Link to="/login">
-            <Button className={buttonNav}>Login</Button>
-          </Link>
-        )}
-        <hr />
+          </li>
+          <li>
+            <Link to="/about">
+              <Button className={buttonNav}>About</Button>
+            </Link>
+          </li>
+          <li>
+            <Link to="/projects">
+              <Button className={buttonNav}>Accomplishments</Button>
+            </Link>
+          </li>
+          <li>
+            <Link to="/blog">
+              <Button className={buttonNav}>Blog</Button>
+            </Link>
+          </li>
+          <li className="navbarGap">
+            {isAuthenticated ? (
+              <Link to="/profile">
+                <Button className={buttonNav}>Profile</Button>
+              </Link>
+            ) : null}
+            {isAuthenticated ? (
+              <Link to="/logout">
+                <Button className={buttonNav}>Logout</Button>
+              </Link>
+            ) : (
+              <Link to="/login">
+                <Button className={buttonNav}>Login</Button>
+              </Link>
+            )}
+          </li>
+        </ul>
       </Breadcrumb>
     </>
   );
